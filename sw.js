350c6d0d0dec1532674df5a2c5074a93df9bf216
francescdevy <erkkrey@gmail.com>
2026-02-23 12:47:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 const CACHE_NAME = 'inventory-v1';
-const ASSETS = ['/', '/index.html'];
+const ASSETS = ['./index.html', './style.css', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(() => {}))
+  );
 });
 
 self.addEventListener('fetch', e => {
